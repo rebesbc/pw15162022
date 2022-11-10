f84c27b12aaa070a09767269f09b2915ffd069b2
e <ccdm18@CCDM18.local>
2022-11-10 15:47:34 -0700

    Random User de múltiples solicitudes

--- randomuser_vue/main.js
+++ randomuser_vue/main.js
@@ -3,7 +3,9 @@ const App = {
         return{
             mensaje: 'Hola Vue.js',
             muestra: false,
-            nombre: ""
+            nombre: "",
+            personas: [],
+            cantidad: 1
         }
     },
     methods:{
@@ -12,12 +14,15 @@ const App = {
         },
         randomuser: async function(){
             let n = ""
-            await axios.get('https://randomuser.me/api/')
+            let p = []
+            await axios.get('https://randomuser.me/api/?results=' + this.cantidad)
             .then(function (response){
                 // console.log(response.data.results[0].name.last)
                 n = response.data.results[0].name.last
+                p = response.data.results
             });
             this.nombre = n
+            this.personas = p
         }
     }
 };
